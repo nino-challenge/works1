a88b3145a042ec47b362a45acbc48574f0578ce3
nino-challenge <83265718+nino-challenge@users.noreply.github.com>
2021-05-02 22:15:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //トップリンク//
 let scrollAnimation = function(){
   const target = document.querySelector(".js-pagetop");
-  if(window.pageYOffset >  900) {
+  if(window.pageYOffset >  2000) {
     target.classList.add("scroll-in");
   } else {
     target.classList.remove("scroll-in");
@@ -22,4 +22,28 @@ document.addEventListener('DOMContentLoaded',function() {
     navArea.classList.toggle("open");
 
   });
+});
+
+//コンタクトフォーム//
+const inp = document.querySelectorAll("input,textarea");
+for(let i=0; i<inp.length; i++) {
+  inp[i].addEventListener("focus",function(){
+    this.nextElementSibling.classList.add("active");
+  });
+  inp[i].addEventListener("blur",function(){
+    this.nextElementSibling.classList.remove("active");
+  });
+};
+
+//スクロールアニメーション//
+const targetElement = document.querySelectorAll(".scAnimation");
+console.log(targetElement);
+document.addEventListener("scroll",() => {
+  for (let i=0 ; i< targetElement.length; i++) {
+    //ブラウザの上側から要素のトップまでの距離を取得
+    const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight*.5;//半分くらい要素が見えたら発動
+    if(window.innerHeight > getElementDistance) {
+      targetElement[i].classList.add("show");
+    } 
+  }
 });
